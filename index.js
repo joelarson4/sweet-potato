8ff3dcf7591e02a58735561376a550fc989d7f62
Larson <larsonj@amazon.com>
2020-03-28 18:45:13 -0700

    Fixing stable genius level goof

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 module.exports = {
-    allMyChildren : require('./allMyChildren');
-    args : require('./args');
-    awaitNewFiles : require('./awaitNewFiles');
-    customStringify : require('./customStringify');
-    dateStuff : require('./dateStuff');
-    log : require('./log');
-    permutator : require('./permutator');
-    readCsv : require('./readCsv');
-    runCommand : require('./runCommand');
-    SeededRandom : require('./SeededRandom');
-    writeCsv : require('./writeCsv');
+    allMyChildren : require('./allMyChildren'),
+    args : require('./args'),
+    awaitNewFiles : require('./awaitNewFiles'),
+    customStringify : require('./customStringify'),
+    dateStuff : require('./dateStuff'),
+    log : require('./log'),
+    permutator : require('./permutator'),
+    readCsv : require('./readCsv'),
+    runCommand : require('./runCommand'),
+    SeededRandom : require('./SeededRandom'),
+    writeCsv : require('./writeCsv')
 }
 
